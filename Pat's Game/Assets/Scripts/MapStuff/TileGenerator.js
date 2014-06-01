2bb6b616ed0ebbee850ac03667e4a5dc62fe96f5
Justin <jhowell518@yahoo.com>
2014-06-01 14:33:58 -0400

    Update 1
    Made basic tile generator and added player movement script.

---
+++ Pat's Game/Assets/Scripts/MapStuff/TileGenerator.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+/************************************
+*		Tile Generator				*
+*	By: Justin Howell / Quill18		*
+*	Date: June 01, 2014 : 1335		*
+*									*
+************************************/
+
+var Tile : GameObject;   //For debugging only!
+var Tiles : GameObject[];
+var CameraSpeed : float;
+public var TotalTiles : int;
+
+function Start() 
+{
+	BuildChunk();
+}
+
+function Update() 
+{
+
+}
+
+function BuildChunk()
+{
+	var MaxY = Mathf.CeilToInt(Camera.main.orthographicSize) * 2;
+	var MaxX = Mathf.CeilToInt(Screen.width / Screen.height * MaxY) * 2;
+
+	for(var x = Mathf.FloorToInt(Camera.main.transform.position.x) - MaxX / 2 ; x < Mathf.CeilToInt(MaxX / 2) + 1; x++) // Calculates the Screen x size
+	{
+		for(var y = Mathf.FloorToInt(Camera.main.transform.position.y) - MaxY / 2; y < Mathf.CeilToInt(MaxY / 2) + 1; y++)  // Calculates the Screen y size
+		{
+			var go = gameObject.Instantiate(Tile);
+			go.transform.position = new Vector3(x, y, 0);
+			go.name = "X: " + x + ", Y: " + y;
+			TotalTiles ++;
+		}
+	}
+}
---
+++ Pat's Game/Assets/Scripts/PlayerMovement.js
@@ -0,0 +1,52 @@
+﻿#pragma strict
+/******************************
+*  		Player Movement		  *
+*  Created by: Justin Howell  *
+*  Date: June 01, 2014 : 1404 *
+*							  *
+******************************/
+
+var Speed : float;
+
+////////Animations///////////
+var Idle : Animation;
+var WalkFront : Animation;
+var WalkLeft : Animation;
+var WalkRight : Animation;
+var WalkBack : Animation;
+///////End Animations/////////
+
+function Awake() 
+{
+	animation.Play("Idle");
+}
+
+function Update() 
+{
+
+	if(Input.GetKey(KeyCode.UpArrow))
+	{
+		transform.position.y += 1 * Time.deltaTime * Speed;
+		animation.Play("WalkFront", PlayMode.StopAll);
+	}
+	if(Input.GetKey(KeyCode.DownArrow))
+	{
+		transform.position.y -= 1 * Time.deltaTime * Speed;
+		animation.Play("WalkBack", PlayMode.StopAll);
+	}
+	if(Input.GetKey(KeyCode.LeftArrow))
+	{
+		transform.position.x -= 1 * Time.deltaTime * Speed;
+		animation.Play("WalkLeft", PlayMode.StopAll);
+	}
+	if(Input.GetKey(KeyCode.RightArrow))
+	{
+		transform.position.x += 1 * Time.deltaTime * Speed;
+		animation.Play("WalkRight", PlayMode.StopAll);
+	}
+	else
+	{
+		animation.Play("Idle", PlayMode.StopAll);
+	}
+	
+}
