a9c0314faabc338034bc5d6510b0bd69cacb3a30
Justin <jhowell518@yahoo.com>
2014-05-31 22:27:13 -0400

    First add
    Added all files for the game.

---
+++ Pat's Game/Assets/Scripts/TileGenerator.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+var Tile : GameObject;   //For debugging only!
+var Tiles : GameObject[];
+var MaxX : int;
+var MaxZ : int;
+public var TotalTiles : int;
+
+function Start () 
+{
+	Build();
+}
+
+function Update () 
+{
+	
+}
+
+function Build()
+{
+	for(var z = 0; z < Mathf.CeilToInt(Camera.main.orthographicSize); z++)
+	{
+		for(var x = 0; x < Mathf.CeilToInt(Screen.width); x++);
+		{
+			Instantiate(Tile, new Vector3(x, 0, z), Quaternion.identity);
+			Debug.Log("X is:" + x);
+			TotalTiles++;
+		}
+	}
+}
